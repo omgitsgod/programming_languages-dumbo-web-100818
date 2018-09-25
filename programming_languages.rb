e35431df5ff6f26490207c42be02f0b76746a017
github username <github email address>
2018-09-25 10:44:44 -0400

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,16 @@
 def reformat_languages(languages)
   # your code here
+  hashy = {}
+
+  languages.each do |k, v|
+    v.each do |x, y|
+      if hashy.has_key?(x)
+        hashy[x][:style] << k
+      else
+        hashy[x] = y
+        hashy[x][:style] = [k]
+    end
+    end
+  end
+  hashy
 end
